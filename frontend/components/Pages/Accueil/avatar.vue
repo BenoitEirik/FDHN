096d7d720d4dea352cda2867205ed72edcfd0a8b
BenoitEirik <olafsergent@gmail.com>
2022-10-15 23:54:45 +0200

    Responvity for avatar

--- frontend/components/Pages/Accueil/avatar.vue
+++ frontend/components/Pages/Accueil/avatar.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    class="h-44 float-left !ml-0 !mt-0 !mr-6 !mb-6 rounded"
+    class="md:w-44 sm:w-36 w-28   float-left !ml-0 !mt-0 !mr-6 !mb-6 rounded"
     src="@/assets/images/brochure/image-004.png"
   >
 </template>
