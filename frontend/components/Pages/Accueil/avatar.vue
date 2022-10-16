4d3dafce010e4a915147e0424db76fd613710c88
BenoitEirik <olafsergent@gmail.com>
2022-10-16 23:53:47 +0200

    Minor changes

--- frontend/components/Pages/Accueil/avatar.vue
+++ frontend/components/Pages/Accueil/avatar.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    class="md:w-44 sm:w-36 w-28   float-left !ml-0 !mt-0 !mr-6 !mb-6 rounded"
+    class="md:w-44 sm:w-36 w-28 float-left !mr-6 !mb-2 rounded"
     src="@/assets/images/brochure/image-004.png"
   >
 </template>
