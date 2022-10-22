578265d3787c8e9c11cbd35e2a9b023f4883170d
BenoitEirik <olafsergent@gmail.com>
2022-10-22 22:24:38 +0200

    Add hover effect on tabs

--- frontend/components/Nav/Link.vue
+++ frontend/components/Nav/Link.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link
     ref="myLink"
-    :class="'p-5 text-xl hover:scale-110 transition-all ease-out text-center ' + (activeDonateClass ? 'decorated':'')"
+    :class="'p-5 text-xl hover:scale-110 transition-all ease-out text-center nuxt-link-effect ' + (activeDonateClass ? 'decorated':'')"
     :to="to"
     @click.native="$nuxt.$emit('check-burger', false)"
   >
@@ -34,7 +34,21 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.nuxt-link-exact-active {
+    color: #eab308;
+}
+.nuxt-link-effect {
+  background: linear-gradient(to right, #eab308 50%, #000 0%);
+  background-size: 200% 200%;
+  background-position: 100%;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  transition: all 300ms;
+}
+.nuxt-link-effect:hover {
+  background-position: 0;
+}
 .decorated {
   text-decoration: underline;
   text-decoration-color: #eab308;
