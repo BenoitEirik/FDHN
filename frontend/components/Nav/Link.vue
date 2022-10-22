a593c6e8dd31ead8c3af287e1db2cc7cecb06e29
BenoitEirik <olafsergent@gmail.com>
2022-10-22 19:13:24 +0200

    Highlight make donation link

--- frontend/components/Nav/Link.vue
+++ frontend/components/Nav/Link.vue
@@ -1,5 +1,6 @@
 <template>
   <nuxt-link
+    ref="myLink"
     class="p-5 text-xl hover:scale-110 transition-all ease-out text-center"
     :to="to"
     @click.native="$nuxt.$emit('check-burger', false)"
@@ -19,12 +20,22 @@ export default {
       type: String,
       default: ''
     }
+  },
+  mounted () {
+    if (this.name.includes('don')) {
+      this.$refs.myLink.$el.classList.add('decorated')
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .nuxt-link-exact-active {
   color: #eab308;
+}
+.decorated {
+  text-decoration: underline;
+  text-decoration-color: #eab308;
+  text-decoration-thickness: 3px;
 }
 </style>
