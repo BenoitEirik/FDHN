cd7cc3c801bd4e2e1b9ae5a4a144afd5ad8566f0
BenoitEirik <olafsergent@gmail.com>
2022-10-24 18:24:39 +0200

    Add more information to user making donation

--- frontend/components/Pages/Dons/StepOne.vue
+++ frontend/components/Pages/Dons/StepOne.vue
@@ -1,11 +1,47 @@
 <template>
   <div class="px-6 py-12 flex flex-col items-center">
-    <div class="px-6 py-12 form-control">
-      <label class="input-group">
-        <span>Don</span>
-        <input ref="amountRef" type="text" placeholder="1" class="w-28 min-w-0 input input-bordered" @change="validateAmount">
-        <span>€</span>
+    <div class="form-control w-full max-w-xs">
+      <!-- Montant -->
+      <label class="label">
+        <span class="label-text">Montant</span>
       </label>
+      <input ref="amountRef" type="text" placeholder="1€ minimum" class="input input-bordered w-full max-w-xs" @change="validateAmount">
+      <label class="label">
+        <span class="label-text-alt">Tous les dons sont défiscalisables</span>
+      </label>
+      <!-- Cause -->
+      <label class="label">
+        <span class="label-text">Cause</span>
+      </label>
+      <select class="select select-bordered">
+        <option selected>
+          L'Homme Nouveau
+        </option>
+        <option>
+          La Scolafricaine
+        </option>
+        <option>
+          Le CIELT
+        </option>
+        <option>
+          L'association Amitié Charles de Foucauld
+        </option>
+      </select>
+      <!-- Nom -->
+      <label class="label">
+        <span class="label-text">Nom</span>
+      </label>
+      <input type="text" placeholder="Nom" class="input input-bordered w-full max-w-xs">
+      <!-- Prénom -->
+      <label class="label">
+        <span class="label-text">Prénom</span>
+      </label>
+      <input type="text" placeholder="Prénom" class="input input-bordered w-full max-w-xs">
+      <!-- Adresse e-mail -->
+      <label class="label">
+        <span class="label-text">Adresse e-mail</span>
+      </label>
+      <input type="email" placeholder="adresse@email.com" class="input input-bordered w-full max-w-xs">
     </div>
   </div>
 </template>
