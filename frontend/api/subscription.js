4cc084e41de858bc49c6c24fd122b3a3dd5b391f
BenoitEirik <olafsergent@gmail.com>
2023-03-01 00:19:01 +0100

    Can get price if existing (avoid create again)

--- frontend/api/subscription.js
+++ frontend/api/subscription.js
@@ -5,6 +5,45 @@ const stripe = require('stripe')(process.env.STRIPE_SK)
 app.use(express.static('public'))
 app.use(express.json())
 
+async function getPrice (amount) {
+  let hasMore = true
+  let pricesData
+  let startingAfter
+  let price
+
+  while (hasMore) {
+    // Request existing prices
+    const prices = await stripe.prices.list({
+      active: true,
+      product: process.env.STRIPE_SUB_PROD_ID,
+      type: 'recurring',
+      ...(startingAfter !== undefined && { starting_after: startingAfter.id })
+    })
+    pricesData = Array.from(prices.data)
+    price = pricesData.find(price => price.unit_amount === amount)
+    if (price !== undefined) {
+      return price
+    }
+
+    if (prices.has_more) {
+      startingAfter = pricesData[pricesData.length - 1]
+      hasMore = prices.has_more
+    } else {
+      break
+    }
+  }
+
+  // Create new price if not found existing
+  return await stripe.prices.create({
+    unit_amount: amount,
+    currency: 'eur',
+    recurring: {
+      interval: 'month'
+    },
+    product: process.env.STRIPE_SUB_PROD_ID // Link with product created on Stripe dashboard
+  })
+}
+
 app.post('/create', async (req, res) => {
   const { description, metadata } = req.body
 
@@ -19,20 +58,14 @@ app.post('/create', async (req, res) => {
     }
   })
 
+  // Search price or create it
+  const price = await getPrice(metadata.amount)
+
   // Create subscription
   const subscription = await stripe.subscriptions.create({
     customer: customer.id,
     items: [
-      {
-        price_data: {
-          unit_amount: metadata.amount,
-          currency: 'eur',
-          recurring: {
-            interval: 'month'
-          },
-          product: process.env.STRIPE_SUB_PROD_ID // Link with product created on Stripe dashboard
-        }
-      }
+      { price: price.id }
     ],
     description: `Don récurrent : ${description}`,
     payment_behavior: 'default_incomplete',
@@ -77,20 +110,14 @@ app.post('/update', async (req, res) => {
   // Delete previous subscription generated
   await stripe.subscriptions.del(id)
 
+  // Search price or create it
+  const price = await getPrice(metadata.amount)
+
   // Recreate subscription
   const subscription = await stripe.subscriptions.create({
     customer: customer.id,
     items: [
-      {
-        price_data: {
-          unit_amount: metadata.amount,
-          currency: 'eur',
-          recurring: {
-            interval: 'month'
-          },
-          product: process.env.STRIPE_SUB_PROD_ID // Link with product created on Stripe dashboard
-        }
-      }
+      { price: price.id }
     ],
     description: `Don récurrent : ${description}`,
     payment_behavior: 'default_incomplete',
