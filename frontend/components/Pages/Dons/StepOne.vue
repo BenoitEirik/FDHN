41953dd9f44bfa78601a12e9b08f58252ab3fbd9
BenoitEirik <olafsergent@gmail.com>
2024-05-07 14:05:31 +0200

    Add button on first step of donation page

--- frontend/components/Pages/Dons/StepOne.vue
+++ frontend/components/Pages/Dons/StepOne.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="px-6 pt-20 pb-12 flex flex-col items-center">
-    <div class="w-full max-w-xs">
-      <div class="btn-group grid grid-cols-2">
+  <div class="flex flex-col items-center px-6 pt-20 pb-12">
+    <div class="flex justify-center w-full">
+      <div class="flex flex-col items-center gap-4 sm:gap-0 sm:flex-row sm:inline-flex sm:btn-group">
         <button ref="btn_simple_donation" class="btn btn-outline btn-accent" @click="donChoice(0)">
           Don unique
         </button>
         <button ref="btn_recurrent_donation" class="btn btn-outline btn-accent" @click="donChoice(1)">
           Don récurrent
+        </button>
+        <button ref="btn_recurrent_donation_2" class="btn btn-outline btn-accent" @click="donChoice(2)">
+          Le Club des mille
         </button>
       </div>
     </div>
@@ -31,22 +34,36 @@ export default {
       if (this.$refs.btn_simple_donation !== undefined && this.$refs.btn_recurrent_donation !== undefined) {
         this.$refs.btn_simple_donation.classList.add('btn-outline')
         this.$refs.btn_recurrent_donation.classList.add('btn-outline')
+        this.$refs.btn_recurrent_donation_2.classList.add('btn-outline')
       }
     },
     donChoice (choice) {
       this.choice = choice
-      if (choice === 0) {
-        // Unique donation
-        this.$refs.btn_simple_donation.classList.remove('btn-outline')
-        this.$refs.btn_recurrent_donation.classList.add('btn-outline')
-        this.$store.commit('setSubscribe', false)
-        this.$emit('can-continue', { value: true })
-      } else {
-        // Recurrent donation
-        this.$refs.btn_simple_donation.classList.add('btn-outline')
-        this.$refs.btn_recurrent_donation.classList.remove('btn-outline')
-        this.$store.commit('setSubscribe', true)
-        this.$emit('can-continue', { value: true })
+      switch (choice) {
+        case 0:
+          // Unique donation
+          this.$refs.btn_simple_donation.classList.remove('btn-outline')
+          this.$refs.btn_recurrent_donation.classList.add('btn-outline')
+          this.$refs.btn_recurrent_donation_2.classList.add('btn-outline')
+          this.$store.commit('setSubscribe', false)
+          this.$emit('can-continue', { value: true })
+          break
+        case 1:
+          // Recurrent donation
+          this.$refs.btn_simple_donation.classList.add('btn-outline')
+          this.$refs.btn_recurrent_donation.classList.remove('btn-outline')
+          this.$refs.btn_recurrent_donation_2.classList.add('btn-outline')
+          this.$store.commit('setSubscribe', true)
+          this.$emit('can-continue', { value: true })
+          break
+        case 2:
+          // Recurrent donation 2
+          this.$refs.btn_simple_donation.classList.add('btn-outline')
+          this.$refs.btn_recurrent_donation.classList.add('btn-outline')
+          this.$refs.btn_recurrent_donation_2.classList.remove('btn-outline')
+          this.$store.commit('setSubscribe', true)
+          this.$emit('can-continue', { value: true })
+          break
       }
     }
   }
